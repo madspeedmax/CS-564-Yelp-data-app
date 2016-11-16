36f56f0e32916d2358f850e04c3191c1da2a8b9d
madspeedmax <hosokawa13@gmail.com>
2016-11-15 21:16:17 -0600

    Sort ranking page, add all three cities

--- YelpApp/Controllers/BusinessRankController.cs
+++ YelpApp/Controllers/BusinessRankController.cs
@@ -21,6 +21,7 @@ namespace YelpApp.Controllers
 
                 #region city 1
                 MySqlCommand cmd = new MySqlCommand();
+                cmd.CommandTimeout = 600;
                 MySqlConnection conn = new MySqlConnection(db.Database.Connection.ConnectionString);
                 conn.Open();
                 cmd.Connection = conn;
@@ -58,6 +59,7 @@ namespace YelpApp.Controllers
 
                 #region city 2
                 MySqlCommand cmd2 = new MySqlCommand();
+                cmd2.CommandTimeout = 600;
                 cmd2.Connection = conn;
                 cmd2.CommandText = "citycategoryscore";
                 cmd2.CommandType = CommandType.StoredProcedure;
@@ -90,6 +92,47 @@ namespace YelpApp.Controllers
                     Score = calc2
                 });
                 #endregion
+
+                #region city 3
+                MySqlCommand cmd3 = new MySqlCommand();
+                cmd3.CommandTimeout = 600;
+                cmd3.Connection = conn;
+                cmd3.CommandText = "citycategoryscore";
+                cmd3.CommandType = CommandType.StoredProcedure;
+                cmd3.Parameters.AddWithValue("@cat", model.Category);
+                cmd3.Parameters["@cat"].Direction = ParameterDirection.Input;
+
+                cmd3.Parameters.AddWithValue("@city", model.City3);
+                cmd3.Parameters["@city"].Direction = ParameterDirection.Input;
+
+                cmd3.Parameters.AddWithValue("@state", model.State3);
+                cmd3.Parameters["@state"].Direction = ParameterDirection.Input;
+
+                cmd3.Parameters.Add("@score", MySqlDbType.Double);
+                cmd3.Parameters["@score"].Direction = ParameterDirection.Output;
+
+                cmd3.ExecuteNonQuery();
+
+                double calc3 = 0;
+                if (cmd3.Parameters["@score"].Value != System.DBNull.Value)
+                {
+                    calc3 = Math.Round((double)cmd3.Parameters["@score"].Value, 2);
+
+                }
+
+                model.RankResults.Add(new BusinessRankResult()
+                {
+                    Category = model.Category,
+                    City = model.City3,
+                    State = model.State3,
+                    Score = calc3
+                });
+                #endregion
+
+                if (model.RankResults != null)
+                {
+                    model.RankResults = model.RankResults.OrderByDescending(r => r.Score).ToList();
+                }
             }
             return View(model);
         }
